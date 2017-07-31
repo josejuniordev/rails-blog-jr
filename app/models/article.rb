93ae1d5ccfb91053e7bfe58b52a823345e85d3ba
José Junior <junioradorart@gmail.com>
2017-07-31 13:00:52 -0300

    validacao de campos vazios

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, msg: 'O título é obrigatório'
+	validates :title, presence: {message: 'O título é obrigatório'},
 			length: {minimum: 5, too_short: 'O titulo é muito curto.'}
 end
