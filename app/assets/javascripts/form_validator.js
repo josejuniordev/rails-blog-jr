fdd1a724de91e79e42260efa4e68205e7f1f7d1f
José Junior <junioradorart@gmail.com>
2017-07-31 13:23:26 -0300

    validacoes do formulario

--- app/assets/javascripts/form_validator.js
+++ app/assets/javascripts/form_validator.js
@@ -1,1 +1,10 @@
-alert('carregou')
+$('[required]').on('blur', function() {
+	var $this = $(this);
+
+	if ($this.val() == '' || $this.val().length < 5) {
+		$this.parent().addClass('has-error')
+
+	} else {
+		$this.parent().removeClass('has-error')
+	}
+})
